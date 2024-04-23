2641fcdbfd1b99ff5925adea1212e6a6c0932d8b
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-23 23:59:43 +0200

    Updated PersistenceController

--- Task Manager/Persistence.swift
+++ Task Manager/Persistence.swift
@@ -10,26 +10,30 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for x in 0..<10 {
-            let newTask = TaskEntity(context: viewContext)
-//            newItem.timestamp = Date()
-            newTask.title = "Task \(x)"
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
+//    static var preview: PersistenceController = {
+//        let result = PersistenceController(inMemory: true)
+//        let viewContext = result.container.viewContext
+//        for x in 0..<10 {
+//            let newTask = TaskEntity(context: viewContext)
+////            newItem.timestamp = Date()
+//            newTask.title = "Task \(x)"
+//        }
+//        do {
+//            try viewContext.save()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nsError = error as NSError
+//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//        }
+//        return result
+//    }()
 
     let container: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "Task_Manager")
