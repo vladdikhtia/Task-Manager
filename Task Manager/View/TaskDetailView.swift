ae293f814512ac23a092df472f0b45028b77f629
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-24 13:05:50 +0200

    Implemented Task detail View with sharing

--- Task Manager/View/TaskDetailView.swift
+++ Task Manager/View/TaskDetailView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct TaskDetailView: View {
+    var task: TaskEntity
+    @State private var newTitle: String = ""
+    @State private var newDescription: String = ""
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var vm: CoreDataViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30){
+            TextField("\(task.title ?? " ") ", text: $newTitle) // \(task.title ?? " ")
+                .padding()
+                .background(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.horizontal)
+                .shadow(color: .white, radius: 8)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            
+            TextField("\(task.taskDescription ?? " ") ", text: $newDescription) // \(task.title ?? " ")
+                .padding()
+                .background(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.horizontal)
+                .shadow(color: .white, radius: 8)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+             
+            
+            Button(
+                action: {
+                    vm.updateTask(task: task, newTitle: newTitle, newDescription: newDescription)
+                    dismiss()
+                }, label: {
+                    Text("Update")
+                        .padding()
+                        .foregroundStyle(.green)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .background(.black)
+                        .clipShape(.rect(cornerRadius: 10))
+                        .shadow(color: .white, radius: 10)
+                        .padding()
+                })
+            
+            Spacer()
+        }
+        .toolbar{
+            ToolbarItem{
+                ShareLink("Share your task", item: "\(task.title ?? "") \n\(task.taskDescription ?? "")")   
+            }
+            
+            ToolbarItem {
+                Button(action: {
+                    withAnimation {
+                        vm.deleteTask(task: task)
+                        dismiss()
+                    } 
+                }, label: {
+                    Image(systemName: "trash")
+                        .resizable()
+                        .imageScale(.large)
+                        .foregroundStyle(.red)
+                })
+            }
+        }
+        
+        
     }
 }
 
-#Preview {
-    TaskDetailView()
-}
+//#Preview {
+//    TaskDetailView(action: .constant {
+//        
+//    })
+//}
