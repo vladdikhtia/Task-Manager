6730eb0f4872c29c6c63d0403548aac52406e269
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-24 13:03:52 +0200

    Implemented Task View with random rotation

--- Task Manager/View/TaskView.swift
+++ Task Manager/View/TaskView.swift
@@ -8,11 +8,86 @@
 import SwiftUI
 
 struct TaskView: View {
+    
+    var task: TaskEntity
+    let index: Int
+    let onDelete: () -> Void
+    let randomColor = Color(
+        .sRGB,
+        red: Double.random(in: 0.5...1),
+        green: Double.random(in: 0.5...1),
+        blue: Double.random(in: 0.5...1),
+        opacity: 1
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .padding(.horizontal, 30)
+                .padding(.vertical, 20)
+                .foregroundStyle(randomColor)
+                .frame(maxWidth: .infinity, maxHeight: 250)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                HStack{
+                    Text("\(task.title ?? "")")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .fontDesign(.rounded)
+                        .foregroundStyle(.background)
+                    
+                     Spacer()
+                    
+                    ShareLink("", item: "\(task.title ?? "") \n\(task.taskDescription ?? "")")
+                }
+                
+                Text("\(task.taskDescription ?? "")")
+                    .font(.headline)
+                    .fontDesign(.rounded)
+                    .foregroundStyle(.background)
+                
+                HStack(alignment: .center) {
+                    Button(action: onDelete) {
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                            .imageScale(.medium)
+                            .padding()
+                            .frame(width: 50, height: 50)
+                            .background(.thickMaterial)
+                            .clipShape(Circle())
+                    }
+                    
+                    Spacer()
+                    
+                        Image(systemName: "greaterthan")
+                            .resizable()
+                            .imageScale(.medium)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .frame(width: 50, height: 50)
+                            .background(.thickMaterial)
+                            .clipShape(Circle())
+                    
+                }
+                
+            }
+            .padding(50)
+            .frame(maxWidth: .infinity, maxHeight: 250, alignment: .topLeading)
+        }
+        .rotationEffect(.degrees(rotationDegree))
     }
+    
+    var rotationDegree: Double {
+            let degree: Double
+            if index % 2 == 0 {
+                degree = .random(in: 0...15)
+            } else {
+                degree = .random(in: -15...0)
+            }
+            return degree
+        }
 }
 
-#Preview {
-    TaskView()
-}
+//#Preview {
+//    TaskView(task: )
+//}
